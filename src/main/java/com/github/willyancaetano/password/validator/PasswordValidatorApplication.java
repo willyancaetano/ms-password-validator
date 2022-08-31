352eea9b72c7f86d014db91d9b700b2e2957967a
Willyan Guimarães Caetano <willyancaetano775@gmail.com>
2022-08-30 21:09:39 -0300

    feat: initial commit

---
+++ src/main/java/com/github/willyancaetano/password/validator/PasswordValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.github.willyancaetano.password.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordValidatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/willyancaetano/password/validator/PasswordValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.willyancaetano.password.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
